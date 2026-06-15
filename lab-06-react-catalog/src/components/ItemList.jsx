7e6dea34a40829a27d99693bce53c962a5a0f0c2
Літвінов Артем Олександрович <a.o.lytvynov@student.khai.edu>
2026-06-15 13:41:42 +0300

    feat(lab-06): add item list

---
+++ lab-06-react-catalog/src/components/ItemList.jsx
@@ -0,0 +1,22 @@
+﻿import ItemCard from "./ItemCard.jsx";
+
+function ItemList({ items, onDelete, onRate }) {
+    if (items.length === 0) {
+        return <p className="empty">Нічого не знайдено.</p>;
+    }
+
+    return (
+        <div className="item-list">
+            {items.map((item) => (
+                <ItemCard
+                    key={item.id}
+                    item={item}
+                    onDelete={onDelete}
+                    onRate={onRate}
+                />
+            ))}
+        </div>
+    );
+}
+
+export default ItemList;
