52ae2269bebcc8ff8d6843413c9137d9b143bc3f
Літвінов Артем Олександрович <a.o.lytvynov@student.khai.edu>
2026-06-15 13:41:01 +0300

    refactor(lab-06): prepare app structure

---
+++ lab-06-react-catalog/src/components/App.jsx
@@ -0,0 +1,10 @@
+﻿function App() {
+    return (
+        <div className="app">
+            <h1>Book Catalog</h1>
+            <p>React catalog app for lab 06.</p>
+        </div>
+    );
+}
+
+export default App;
